57828983c2843c5075645acfd4a0081965855167
Khaled Nadam <khalednadam@gmail.com>
2024-07-20 10:11:09 +0300

    chore: delete unused component

--- src/app/components/Button.tsx
+++
@@ -1,8 +0,0 @@
-import { cva } from "class-variance-authority";
-import React from "react";
-
-const Button = ({ children }: Readonly<{ children: React.ReactNode }>) => {
-  return <button>{children}</button>;
-};
-
-export default Button;
